a883c3e4ce51d5bf0c95d70e62698ac2945eecae
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-08 12:53:02 -0700

    removed start query in exchange for 5 year limit

--- src/api/yahoo.ts
+++ src/api/yahoo.ts
@@ -2,7 +2,9 @@ import {Request, Response} from 'express'
 let yahooFinance = require('yahoo-finance');
 
 var today = new Date()
-var currentDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+var currentYear = today.getFullYear()
+var currentDate = currentYear +'-'+(today.getMonth()+1)+'-'+today.getDate()
+var PastDate = (currentYear-5) +'-'+(today.getMonth()+1)+'-'+today.getDate()
 
 export default async function handler (req: Request, res: Response) {
   if (!req.query || !req.query.symbol)
@@ -10,7 +12,7 @@ export default async function handler (req: Request, res: Response) {
   else {
     await yahooFinance.historical({
       symbol: req.query.symbol,
-      from: '2015-10-01',
+      from: PastDate,
       to: currentDate,
       period: req.query.period
     }, function (err: any, quotes: [{}]) {
