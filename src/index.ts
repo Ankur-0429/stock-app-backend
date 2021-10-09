718a5f8df00938a4544880e0ca83fee92f11e121
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-09 08:39:25 -0700

    changed url for backend vercel

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ const port: Number = 5000;
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://stock-app-alpha.vercel.app/',
     credentials: true
 }))
+
+// @ts-ignore
+app.get('/', (req: Request, res: Response)=>{
+    res.send('home')
+})
 
 // @ts-ignore
 app.get('/api/stocks/:year/query', (req: Request,res: Response)=> {
